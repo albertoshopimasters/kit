d5269101fbe4751fef0a9fc4021d6b6cf114d33b
pablokolls <pablo.koll@shopimasters.es>
2025-08-21 00:20:19 +0200

    fix: lint errors

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,19 +1,19 @@
-import { useEffect } from "react";
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { useFetcher } from "@remix-run/react";
+import { TitleBar, useAppBridge } from "@shopify/app-bridge-react";
 import {
-	Page,
-	Layout,
-	Text,
-	Card,
-	Button,
 	BlockStack,
 	Box,
-	List,
-	Link,
+	Button,
+	Card,
 	InlineStack,
+	Layout,
+	Link,
+	List,
+	Page,
+	Text,
 } from "@shopify/polaris";
-import { TitleBar, useAppBridge } from "@shopify/app-bridge-react";
+import { useEffect } from "react";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -113,7 +113,7 @@ export default function Index() {
 	return (
 		<Page>
 			<TitleBar title="Remix app template">
-				<button variant="primary" onClick={generateProduct}>
+				<button type="submit" variant="primary" onClick={generateProduct}>
 					Generate a product
 				</button>
 			</TitleBar>
